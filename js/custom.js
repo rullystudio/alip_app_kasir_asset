57d645e201858aacc390fe291e6162e627a14866
Rully <rully.winarto@gmail.com>
2022-06-25 09:34:03 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,13 @@
 
+  (function($) {
+  'use strict';
+  $(function() {
+    $('#browse-file').on('click', function() {
+      var file = $(this).parent().parent().parent().find('#upload-default');
+      file.trigger('click');
+    });
+    $('#upload-default').on('change', function() {
+      $(this).parent().find('.foto-barang').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+    });
+  });
+})(jQuery);
